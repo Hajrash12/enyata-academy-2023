1a0f258107c5e0f6707d7d704fcef8f9342d43f0
Hajrash12 <awalmohammedhajara@gmail.com>
2023-05-26 03:37:29 -0700

    assignment

---
+++ Intro_to_js/index.js
@@ -0,0 +1,20 @@
+let myName = "Hajara";
+
+typeof myName;
+
+myName = "Awal";
+
+let car = {
+    make: "Toyota",
+    mode: "Venza",
+    year: "2020",
+    color: "White",
+    mileage: 2000,
+    fuelType: "Petrol",
+    engineSize: 1.8,
+    isAutomatic: true,
+    owners: ["Mark", "Rashid", "Eric" ],
+    features:["Bluetooth Touch Screen","Back Camera"]
+};
+
+}
---
+++ Intro_to_js/numbers.js
@@ -0,0 +1,9 @@
+const favoriteNumber = 7;
+const halfOfFavNum = favoriteNumber / 2;
+let isaacsFavoritesFavorite = 13;
+
+let difference = isaacsFavorite - favoriteNumber;
+
+isaacsFavorite = isaacssFavorite * 26;
+
+isaacssFavorite = isaacsFavorite % 3;
